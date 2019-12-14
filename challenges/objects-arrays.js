2078620905c1fd307f7e8975780f4c1ce1e24156
Jeff Clark <jeffclarktopeka@yahoo.com>
2019-12-13 20:19:43 -0600

    finished object challenges

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,6 +6,14 @@
   object name, diet, weight, length, period
 */
 
+const dinosaur = {
+  name: name,
+  diet: diet,
+  weight: weight,
+  length: length,
+  period: period,
+}
+
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
@@ -15,20 +23,21 @@
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dinosaur.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dinosaur.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dinosaur.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dinosaur.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+dinosaur.roar = 'RAWERSRARARWERSARARARRRR!'
+console.log(dinosaur.roar);
 
 
 // ==== Arrays ====
