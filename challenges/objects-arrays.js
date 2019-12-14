12a6b195bde5ff5afb725727ab8e17df4d4514a7
Jeff Clark <jeffclarktopeka@yahoo.com>
2019-12-13 23:33:34 -0600

    finished Sprint

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -131,9 +131,9 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = [`Name: ${animal_name}, Scientific: ${scientific_name}`];
-displaysNames.forEach( displayNames =>
-console.log(displayNames));
+const displayNames = [];
+displayNames.forEach(`Name: ${animal_name}, Scientific: ${scientific_name}`)
+console.log(displayNames);
 
 /* Request 2: .map()
 
@@ -141,8 +141,9 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [`${animal_name}`];
-lowCaseAnimalNames.map(animal_names.toLowerCase)
+const lowCaseAnimalNames = [];
+zooAnimals.map(`${animal_name}`);
+return animal_name.toLowerCase();
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -150,8 +151,8 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [{population}];
-lowPopulationAnimals.filter(population < 5)
+const lowPopulationAnimals = [];
+zooAnimals.filter(zooAnimals.population < 5)
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -160,7 +161,9 @@ The zoos need to know their total animal population across the United States. Fi
 
 */
 const populationTotal = 0;
-populationTotal.reduce()
+zooAnimals.reduce(function (population, zooAnimal, index, zooAnimals) {
+  return population + zooAnimal.population;
+}, 0);
 console.log(populationTotal);
 
 
